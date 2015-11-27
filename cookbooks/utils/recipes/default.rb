2828182b1e09d6dac0a369240026629900b4b801
Greg K <grifkon@gmail.com>
2015-11-28 00:45:18 +0200

    Fixed Utils again...

--- cookbooks/utils/recipes/default.rb
+++ cookbooks/utils/recipes/default.rb
@@ -7,6 +7,11 @@ include_recipe 'apt'
 package 'vim'
 package 'aptitude'
 package 'deluged'
+
+service 'deluged' do
+  supports :status => true
+  action [:enable, :start]
+end
 package 'deluge'
 package 'deluge-webui'
 package 'deluge-console'
