62f2d909e32b0aedd26b1d97b3dfc90fa71a77ae
Greg K <grifkon@gmail.com>
2015-11-28 00:15:41 +0200

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@ if [[ $EUID -ne 0 ]]; then
  echo -e "Please run as root\n" 
  exit 1
 fi
-echo -e "Downloading ChefDK v. 0.10.0-1_amd64. Please wait...\n"
-wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.10.0-1_amd64.deb -o /usr/src/chefdk_0.10.0-1_amd64.deb
-echo -e "ChefDK saved to /usr/src/chefdk_0.10.0-1_amd64.deb\n Installing CHefDK..."
+#echo -e "Downloading ChefDK v. 0.10.0-1_amd64. Please wait...\n"
+#wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.10.0-1_amd64.deb -o /usr/src/chefdk_0.10.0-1_amd64.deb
+#echo -e "ChefDK saved to /usr/src/chefdk_0.10.0-1_amd64.deb\n Installing CHefDK..."
 
-dpkg -i /usr/src/chefdk_0.10.0-1_amd64.deb
-echo -e "Installed.\nApplying cookbooks..."
-
-sudo chef-client --local-mode --runlist 'recipe[scripts]'
-sudo chef-client --local-mode --runlist 'recipe[system]'
-sudo chef-client --local-mode --runlist 'recipe[google-chrome]'
+#dpkg -i /usr/src/chefdk_0.10.0-1_amd64.deb
+#echo -e "Installed.\nApplying cookbooks..."
+for cookbook in `ls ./cookbooks/`; do
+ echo "sudo chef-client --local-mode --runlist 'recipe[$cookbook]'";
+done
