1a3e2984bf94bdb4d8a9e87652d062098d3dfb8b
Greg K <grifkon@gmail.com>
2015-11-28 00:26:52 +0200

    Added a script to Scripts

--- cookbooks/scripts/recipes/default.rb
+++ cookbooks/scripts/recipes/default.rb
@@ -21,3 +21,10 @@ template '/usr/local/bin/iPhone_bak_files_renamer.sh' do
   group 'grifonas'
   mode '0775'
 end
+
+template '/usr/local/bin/mkv-remove-extra-tracks.sh' do
+  source 'mkv-remove-extra-tracks.sh.erb'
+  owner 'grifonas'
+  group 'grifonas'
+  mode '0775'
+end
--- cookbooks/utils/recipes/default.rb
+++ cookbooks/utils/recipes/default.rb
@@ -5,3 +5,11 @@
 include_recipe 'apt'
 
 package 'vim'
+package 'aptitude'
+package 'deluged'
+package 'deluge'
+package 'deluge-webui'
+package 'deluge-console'
+package 'mencoder'
+package 'mkvmerge'
+package 'mplayer'
