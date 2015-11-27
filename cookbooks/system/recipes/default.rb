d9629a844f8ce369554669348932321ae34c85f7
Greg K <grifkon@gmail.com>
2015-11-27 23:41:43 +0200

    Change locale from templates to content

--- cookbooks/system/recipes/default.rb
+++ cookbooks/system/recipes/default.rb
@@ -4,10 +4,21 @@
 #
 # Copyleft November 2015; Greg K.
 
-template '/etc/default/locale' do
-  source locale.erb
+#template '/tmp/locale' do
+ # source locale.erb
+file 'tmp/locale' do 
   owner 'root'
   group 'root'
   mode '0644'
+  content 'LANG="en_GB.UTF-8"
+LC_NUMERIC="en_GB.UTF-8"
+LC_TIME="en_GB.UTF-8"
+LC_MONETARY="en_GB.UTF-8"
+LC_PAPER="en_GB.UTF-8"
+LC_NAME="en_GB.UTF-8"
+LC_ADDRESS="en_GB.UTF-8"
+LC_TELEPHONE="en_GB.UTF-8"
+LC_MEASUREMENT="en_GB.UTF-8"
+LC_IDENTIFICATION="en_GB.UTF-8"'
 end
 
