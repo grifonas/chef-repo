1b03f8fb0bf3f12c86e0cd5b179c1d888924199c
Greg K <grifkon@gmail.com>
2015-12-01 22:00:19 +0200

    ...

--- cookbooks/scripts/recipes/default.rb
+++ cookbooks/scripts/recipes/default.rb
@@ -10,25 +10,3 @@ include_recipe 'iPhone_bak_files_renamer.rb'
 include_recipe 'mkv-remove-extra-tracks.rb'
 include_recipe 'treesize.rb'
 
-template '/usr/local/bin/treesize.sh' do
-  source 'treesize.sh.erb' 
-  owner 'grifonas'
-  group 'grifonas'
-  mode '0775'
-end
-
-package 'libimage-exiftool-perl'
-
-template '/usr/local/bin/iPhone_bak_files_renamer.sh' do
-  source 'iPhone_bak_files_renamer.sh.erb'
-  owner 'grifonas'
-  group 'grifonas'
-  mode '0775'
-end
-
-template '/usr/local/bin/mkv-remove-extra-tracks.sh' do
-  source 'mkv-remove-extra-tracks.sh.erb'
-  owner 'grifonas'
-  group 'grifonas'
-  mode '0775'
-end
