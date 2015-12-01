4e5a4e5e814ccfc4ed961502d6e160004c6d9bec
Greg K <grifkon@gmail.com>
2015-12-01 22:17:30 +0200

    ..

--- cookbooks/scripts/metadata.rb
+++ cookbooks/scripts/metadata.rb
@@ -4,5 +4,6 @@ maintainer_email 'grifkon@gmail.com'
 license 	'all_rights'
 description 	'Installs/Configures scripts'
 long_description 'Installs/Configures scripts'
-depends		'apt'
-version 	'0.1.1'
+version 	'0.1.2'
+
+depends	'apt', '~> 2.9.2'
