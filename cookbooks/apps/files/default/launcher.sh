cead9e390ac484eaf9a2b36271ac82afe1df3a0f
Greg K <grifkon@gmail.com>
2015-12-04 23:37:40 +0200

    Added Skyoe to launcher

--- cookbooks/apps/files/default/launcher.sh
+++ cookbooks/apps/files/default/launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://firefox.desktop', 'application://org.gnome.Nautilus.desktop', 'application://deluge.desktop', 'application://gnome-terminal.desktop', 'unity://running-apps', 'unity://devices', 'unity://expo-icon']"
+/usr/bin/gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://firefox.desktop', 'application://org.gnome.Nautilus.desktop', 'application://deluge.desktop', 'application://gnome-terminal.desktop', 'application://skype.desktop', 'unity://running-apps', 'unity://devices', 'unity://expo-icon']"
