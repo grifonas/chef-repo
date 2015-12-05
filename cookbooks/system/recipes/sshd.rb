00d8ec05258515422737ab979bd1f5488f956e94
Greg K <grifkon@gmail.com>
2015-12-05 19:58:23 +0200

    fixed more bugs in sshd.

--- cookbooks/system/recipes/sshd.rb
+++ cookbooks/system/recipes/sshd.rb
@@ -6,8 +6,6 @@
 
 openssh_server '/etc/ssh/sshd_config' do
   Port 6758
-  AllowUsers 'grifonas@52.16.55.4'
-  AllowUsers 'grifonas@localhost'
-  AllowUsers 'grifonas@10.0.0.1/24'
+  AllowUsers 'grifonas@52.16.55.4 grifonas@localhost grifonas@10.0.0.1/24'
 end
   
