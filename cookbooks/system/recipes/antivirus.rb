6212bf24eb2e672fce10b38599c37b5ee4e1e183
Greg K <grifkon@gmail.com>
2016-01-01 09:32:32 +0200

    .

--- cookbooks/system/recipes/antivirus.rb
+++ cookbooks/system/recipes/antivirus.rb
@@ -16,7 +16,7 @@ package 'clamav-daemon'
 cron 'update_and_scan' do
   hour '4'
   minute '30'
-  command 'echo `date` > /var/log/clamav/clamreport.log && /usr/bin/clamscan --log=/var/log/clamav/clamreport.log --recursive --infected --exclude=/proc --exclude=/dev/ --exclude=/sys /'
+  command 'echo `date` > /var/log/clamav/clamreport.log && /usr/bin/clamscan --log=/var/log/clamav/clamreport.log --recursive --infected --exclude=/proc --exclude=/dev/ --exclude=/sys / && echo `date` >> /var/log/clamav/clamreport.log'
 end
 
 
