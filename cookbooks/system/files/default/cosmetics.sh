17b3faf5dc05d2eada776fb903f31671a22547d3
Greg K <grifkon@gmail.com>
2016-01-04 21:47:57 +0200

    .

--- cookbooks/system/files/default/cosmetics.sh
+++ cookbooks/system/files/default/cosmetics.sh
@@ -2,3 +2,4 @@
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 2
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/building.jpg
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb'), ('xkb', 'il')]"
